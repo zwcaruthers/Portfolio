3d5d04fe006aa8e39b7d50ab4f7ea169d20a5ab4
zwcaruthers <zwcaruthers@gmail.com>
2022-08-25 17:06:00 -0400

    Added mobile menu and placeholder text

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,15 @@
-const navToggle = document.querySelector('#navToggle');
+const menu = document.querySelector('#menu');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
-const hamburger = document.querySelector('#hamburger');
 
-// hamburger
 
-navToggle.addEventListener('click', () => {
+// mobile menu icon
+
+menu.addEventListener('click', () => {
     nav.classList.toggle('open');
     navIcon.forEach(icon => {
         icon.classList.toggle('hidden');
     });
 });
 
-// hamburger-resize of window after open before closing
 
-window.addEventListener("resize", () => {
-    if(document.body.clientWidth > 720) {
-    nav.classList.remove('open');
-    navIcon.forEach(icon => {
-        icon.classList.add('hidden')
-    });
-
-    hamburger.classList.remove('hidden');
-
-    }});
